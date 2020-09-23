0ada7624e51701e9d89690238b012b35a9676b4e
jbandy98 <jbandy98@gmail.com>
2020-09-23 10:54:50 -0500

    Initial creation of project and repo.

---
+++ src/main/java/com/techinvest/onlineresume/OnlineresumeApplication.java
@@ -0,0 +1,13 @@
+package com.techinvest.onlineresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineresumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineresumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techinvest/onlineresume/OnlineresumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techinvest.onlineresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineresumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
